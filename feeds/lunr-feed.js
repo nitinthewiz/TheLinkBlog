b360c6703f3ec8a1adb23c0b44b6d772fdbb5e88
Jamie Thingelstad <jamie@thingelstad.com>
2017-09-28 21:04:47 -0500

    Search working now.
    Showing results in a nice format that is very usable. This is
    functional now.

--- feeds/lunr-feed.js
+++ feeds/lunr-feed.js
@@ -3,21 +3,28 @@ layout: null
 ---
 // builds lunr
 
+{% assign count = 0 %}
 var links = [{
 {% for post in site.posts %}
+  "id": {{count}},
   "title": {{post.title | jsonify }},
   "url": {{post.url | jsonify }},
   "external_url": {{post.external-url | jsonify }},
   "date": {{ post.date | date: '%B %-d, %Y' | jsonify }},
+  "year": {{ post.year }},
+  "month": {{ post.month }},
   "excerpt": {{ post.content | strip_html | truncatewords: 20 | jsonify }}
-}{% unless forloop.last %}, { {% endunless %}{% endfor %}]
+}{% unless forloop.last %}, { {% endunless %}{% assign count = count | plus: 1 %}{% endfor %}]
 
 
 var index = lunr(function () {
-  this.ref('title')
+  this.ref('id')
+  this.field('title')
   this.field('url')
   this.field('external_url')
   this.field('date')
+  this.field('year')
+  this.field('month')
   this.field('excerpt')
 
   links.forEach(function (doc) {
@@ -42,8 +49,13 @@ $(document).ready(function() {
 
     // Loop through, match, and add results
     for (var item in result) {
-      // var searchitem = '<div class="result"><div class="result-body"><a href="'+result[item].url+'" class="post-title">'+result[item].title+'</a><div class="post-date small">'+result[item].date+'</div><p>'+result[item].excerpt+'</p></div>';
-      var searchitem = '<div class="result">'+result[item].ref+' ('+result[item].score+')</div>';
+      var searchitem = '<article class="post">'
+      searchitem += '<div class="post-confidence">score = '+result[item].score+'</div>';
+      searchitem += '<h1><a href="'+links[result[item].ref].external_url+'" onclick="trackOutboundLink(\''+links[result[item].ref].external_url+'\'); return false;">'+links[result[item].ref].title+'</a>&nbsp;<a class="anchor" href="'+links[result[item].ref].url+'"><i class="fa fa-paper-plane" aria-hidden="true"></i></a></h1>';
+      searchitem += '<div class="post-date">'+links[result[item].ref].date+'</div>';
+      searchitem += '<div class="post-content">'+links[result[item].ref].excerpt+'</div>';
+      searchitem += '</article>';
+
       resultdiv.append(searchitem);
     }
 
